45bc72831a7b83de03359a9fa2c9a91a209db25b
Martynaxyz <martyna.olga.wojciechowska@gmail.com>
2023-06-28 21:47:13 +0200

    added file

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //dodawanie, odejmowanie, mnożenie, dzielenie
+        //https://orange-java.pl.sdacademy.pro/zadania/testowanie_oprogramowania_-_podstawy/testowanie_oprogramowania_-_podstawy/
+
     }
 }
