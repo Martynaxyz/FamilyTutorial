8db423fe2b6b1e85096899d937b8ba85950542d0
Martynaxyz <martyna.olga.wojciechowska@gmail.com>
2023-06-28 22:20:34 +0200

    print variables

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -2,5 +2,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Franek Wojciechowski -> jak będzie cięzko trenował to będzie lepszy niż Lewandowski");
+        int x = 3;
+        int y= 2;
+        int z = 1;
+        System.out.println("x y z");
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(x+y+z);
+
     }
 }
